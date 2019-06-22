fe58e30c2960a201616d461d44def7b2c80d8081
Rogerio Sobrinho <rogerioa.sobrinho@gmail.com>
2019-06-22 17:32:10 -0300

    app created by react-native using rocketset template

---
+++ src/components/repository/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import {
+  Container, Name, Description, Stats, Stat, StatCount, Refresh, RefreshText,
+} from './styles';
+
+export default function repository({ data, onRefresh }) {
+  return (
+    <Container>
+      <Name>{data.name}</Name>
+      <Description>{data.description}</Description>
+      <Stats>
+        <Stat>
+          <Icon name="star" dize={16} color="#333" />
+          <StatCount>{data.stars}</StatCount>
+        </Stat>
+        <Stat>
+          <Icon name="code-fork" dize={16} color="#333" />
+          <StatCount>{data.forks}</StatCount>
+        </Stat>
+      </Stats>
+
+      <Refresh onPress={onRefresh}>
+        <Icon name="refresh" color="#7159c1" size={16} />
+        <RefreshText>ATUALIZAR</RefreshText>
+      </Refresh>
+    </Container>
+  );
+}
